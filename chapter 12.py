7fe058e16b12f3c3cf17962aa14f09b83c7090c2
1bamboo1 <96080154+1bamboo1@users.noreply.github.com>
2022-04-22 16:59:26 -0500

    Add files via upload

---
+++ chapter 12.py
@@ -0,0 +1,182 @@
+Python 3.10.4 (v3.10.4:9d38120e33, Mar 23 2022, 17:29:05) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: /Users/brandon/Desktop/code/run.py =================
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+Enter your choice: 5
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+Enter your choice: 5
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+Enter your choice: 5
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+Enter your choice: 
+================= RESTART: /Users/brandon/Desktop/code/run.py =================
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 5
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 5
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 5
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 
+================= RESTART: /Users/brandon/Desktop/code/run.py =================
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+Enter your choice: 
+================= RESTART: /Users/brandon/Desktop/code/run.py =================
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 5
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+5
+Enter your choice: 
+Traceback (most recent call last):
+** IDLE Internal Exception: 
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/idlelib/run.py", line 578, in runcode
+    exec(code, self.locals)
+  File "/Users/brandon/Desktop/code/run.py", line 8, in <module>
+    choice = int(input("Enter your choice: "))
+ValueError: invalid literal for int() with base 10: ''
+
+================= RESTART: /Users/brandon/Desktop/code/run.py =================
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 1
+enter first name10
+enter second number:10
+20
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 10
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 2
+enter first name
+================= RESTART: /Users/brandon/Desktop/code/run.py =================
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 1
+enter first number20
+enter second number:20
+40
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 2
+enter first number10
+enter second number:10
+0
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 3
+enter first number30
+enter second number:40
+70
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 
+================= RESTART: /Users/brandon/Desktop/code/run.py =================
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 1
+enter first number20
+enter second number:20
+40
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 2
+enter first number20
+enter second number:20
+0
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 3
+enter first number30
+enter second number:40
+70
+incorrect  choice
+1,addition of two numbers
+2, difference between two numbers
+2, multiplication of two numbers
+4, exit
+
+Enter your choice: 
